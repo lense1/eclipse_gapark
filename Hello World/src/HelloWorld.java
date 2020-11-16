09ea20d818fd0c3d6f7e6db5513135259fef124d
GyeongAn Park <star_ruddks@naver.com>
2020-11-16 21:29:30 +0900

    added print student number

--- Hello World/src/HelloWorld.java
+++ Hello World/src/HelloWorld.java
@@ -4,6 +4,9 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Park GyeongAn");
+		System.out.println("20192755");
+		
+		
 	}
 
 }
