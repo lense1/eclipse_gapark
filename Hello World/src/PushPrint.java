600c3c8aa16ec484c6d70d4c911e9a546141d24d
lense1 <star_ruddks@naver.com>
2020-11-18 19:54:02 +0900

    PushPrint.java modified on GitHub

--- Hello World/src/PushPrint.java
+++ Hello World/src/PushPrint.java
@@ -4,7 +4,7 @@ public class PushPrint {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("sophomore");
-		
+		System.out.println("Jeonbuk Iksan");
 	}
 
 }
